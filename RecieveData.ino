5931e9a30d7cd269113582bdf3652313a31ca960
vincentlam-coder <vincentlam360@gmail.com>
2026-04-26 21:03:27 -0400

    Create ReceiveData.ino
    Arduino code to receive data from python script

---
+++ RecieveData.ino
@@ -0,0 +1,59 @@
+#include <Servo.h>
+
+Servo servo;
+
+int angles[6] = {0,0,0,0,0,0};
+
+void setup() {
+  //put your setup code here, to run once:
+  //Braccio.begin();
+  //Braccio.ServoMovement(20, 90, 90, 90, 90, 90, 73);
+  servo.attach(7);
+  Serial.begin(9600);
+  servo.write(0);
+  delay(1000);
+  servo.write(180);
+}
+
+void loop() {
+  //put your main code here, to run repeatedly:
+  if (Serial.available() > 0)
+  {
+    String data = Serial.readStringUntil('\n');
+    update_angles(data);
+    servo.write(angles[5]);
+  }
+}
+
+//function used to update angles based on string read
+void update_angles(String data)
+{ 
+  int length = data.length();
+  int i = 0; //index for string
+
+  //outer for loop iterates through angles
+  for (int j = 0; j < 6; j++)
+  {
+    //inner for loop iterates through data string
+    for (int k = i + 1; k < length; k++)
+    {
+      if (data[k] == ',')
+      { 
+        if (k - i == 1) //single digit
+        {
+          char q = data[i]; //extract angle from string
+          angles[j] = q - '0'; //convert to int + update angles array
+        }
+        else
+        {
+          String q = data.substring(i,k); //extract angle from string
+          angles[j] = q.toInt(); //convert to int + update angles array
+        }
+        i = k + 1; //update index
+        break;
+      }
+    }
+  }
+}
+
+

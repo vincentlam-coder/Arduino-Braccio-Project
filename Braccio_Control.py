191eae12feb284e9fd81a1b5c686bbc8e0c2979d
vincentlam-coder <vincentlam360@gmail.com>
2026-05-17 18:38:01 -0400

    Update Braccio_Control.py

--- Braccio_Control.py
+++ Braccio_Control.py
@@ -1,10 +1,7 @@
-import scipy
-import sympy
 import numpy as np
-import math
 from numpy import linalg as LA
 from numpy.linalg import pinv
-from math import pi,sin,cos
+from math import pi,sin,cos,atan2,sqrt,radians,degrees
 
 np.set_printoptions(precision=3,suppress=True)
 
@@ -82,10 +79,10 @@ class Braccio():
 
         return linear_jacobian
 
-    #method to compute inverse kinematics
-    def IK(self,target_pos,num_pts,counter_limit):
+    #method to compute inverse kinematics numerically
+    def IK(self,target_pos,num_pts,counter_limit,q_init = [0,-120,60,115,0]):
         error_threshold = 0.01 #define error threshold in m
-        q_init = [0,-120,60,115,0] #robot initial configuration
+        #q_init = [0,-120,60,115,0] #robot initial configuration
         q_init = np.radians(q_init) #convert from degrees to radians
         T_init = self.FK(q_init)
         init_pos = T_init[0:3,3] 
@@ -135,25 +132,90 @@ class Braccio():
 
         return T_cur, q_vector, error
 
-    #method for q remapping to servo angles
+    def limit(self,value,min,max):
+        if (value < min):
+            return min
+        elif (value > max):
+            return max
+        else:
+            return value
+
+    #geometrical inverse kinematics with joint limit constraints
+    def geo_IK(self,x,y,z):
+        success = False #flag variable for IK success
+        #define link lengths (m)
+        l1 = 0.0715
+        l2 = 0.125
+        l3 = 0.125
+        l4 = 0.192
+        #x-y plane
+        q1rad = atan2(y,x)
+        q1 = degrees(q1rad)
+        r = x*x + y*y 
+        r = sqrt(r)
+        s = z - l1
+        a_y = -1
+        b_y = -1
+        EE_y = -1
+        #arm plane
+        for phi in range(-90,1): #iterate through valid phi in degrees
+            phi_rad = radians(phi) #convert phi from degrees to radians
+            x_prime = r - l4*cos(phi_rad)
+            y_prime = s - l4*sin(phi_rad)
+            d = sqrt(x_prime*x_prime + y_prime*y_prime)
+            #check if (x',y') is within workspace limits
+            if (d > (l2 + l3) or (d < abs(l2 - l3))):
+                continue
+            #if (x',y') is reachable -> solve for q2,q3,q4 for elbow down config
+            #q3
+            D = ((d**2) - (l2**2) - (l3**2))/(2*l2*l3)
+            q3rad = atan2(-sqrt(1 - (D**2)),D)
+            q3 = degrees(q3rad)
+            if (q3 != self.limit(q3,-90,90)):
+                continue
+            #q2
+            a = l2 + l3*cos(q3rad)
+            b = l3*sin(q3rad)
+            term1 = atan2(y_prime,x_prime)
+            term2 = atan2(b,a)
+            q2rad = term1 - term2
+            q2 = degrees(q2rad)
+            if (q2 != self.limit(q2,15,165)):
+                continue
+            #q4
+            q4 = phi - q2 - q3
+            if (q4 != self.limit(q4,-90,90)):
+                continue
+            #check if robot arm crashes into ground
+            a_y = l1 + l2*sin(q2rad) #pt a y position
+            if ((a_y < 0) or (a_y == 0)):
+                continue
+            b_y = a_y + l3*sin(q2rad + q3rad) #pt b y position
+            if ((b_y < 0) or (b_y == 0)):
+                continue
+            q4rad = radians(q4)
+            EE_y = b_y + l4*sin(q2rad + q3rad + q4rad) #EE y position
+            if (EE_y < 0):
+                continue
+            #remap angles to physical servo angles + update flag variable
+            success = True
+            q1 = -q1 + 90
+            q2 = -q2 + 180
+            q3 = -q3 + 90
+            q4 = -q4 + 90
+            break
+        
+        q = [90,90,90,90,90] #home config if IK fails
+        if (success == True):
+            q = [q1,q2,q3,q4,90]
+
+        return success,q,phi,a_y,b_y,EE_y
+
+    #method for q remapping to servo angles for numerical IK
     def q_remapping(self,q_vector):
-        q_vector = np.rad2deg(q_vector) #convert to degrees
         q_vector[0] = -q_vector[0] + 90 #M1
         q_vector[1] = q_vector[1] + 180 #M2
         q_vector[2] = q_vector[2] + 90 #M3
         q_vector[4] = q_vector[4] + 90 #M5
 
         return q_vector
-
-robot = Braccio(0.0715,0.125,0.125,0.192)
-target_pos = [0.1,0.35,0.2]
-counter_limit = 70
-num_pts = 15
-
-T,q,error = robot.IK(target_pos,num_pts,counter_limit)
-
-T_actual = robot.FK(q)
-print('position -> ', T_actual[0:3,3])
-print('error -> ', error)
-qdeg = np.rad2deg(q)
-print('q -> ', qdeg)
